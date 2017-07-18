bb3d13594d81ff78aa76e9c8bf6454bd20e80e1c
Navin Kumar <navinspm@gmail.com>
2017-07-18 14:19:40 +0530

    Added total number method

--- get_kfit.rb
+++ get_kfit.rb
@@ -1,10 +1,22 @@
 require 'mechanize'
 require 'csv'
 
-def Scraper
+class Scraper
 	def initialize
      @base_url = 'https://access.kfit.com/'
      @city = 'kuala-lumpur'
      @mechanize = Mechanize.new
 	end
+
+	def get_total_pages
+		url = "#{@base_url}partners?city=#{@city}"
+		page = @mechanize.get(url)
+		total_pages = page.search('.pagination li:last a')[0]['href'].split('=')[2]
+	end
 end
+
+scrape = Scraper.new
+p 'Getting total number of pages from pagination'
+total_pages = scrape.get_total_pages
+p '-------------------------------------------'
+p "Total Pages - #{total_pages}"
